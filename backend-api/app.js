acb7812b97b1e1fe938a35ddc4f1fbcb84c92339
Om Patel <o.v.patel2705@gmail.com>
2023-08-09 23:53:25 +0530

    Created API

---
+++ backend-api/app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+
+const app = express();
+
+const dbURL = "mongodb+srv://sherlock:sherlocked221b@cluster0.cfth3qm.mongodb.net/bmp";
+
+const cities = [];
+
+mongoose.connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+
+    const Schema = mongoose.Schema;
+
+    const schema = new Schema({
+        city: {
+            type: String,
+        },
+        country: {
+            type: String,
+        },
+        population: {
+            type: Number,
+        },
+        area: {
+            type: Number,
+        },
+        density: {
+            type: Number,
+        },
+        gdp: {
+            type: Number,
+        },
+        climate: {
+            type: String,
+        },
+        language: {
+            type: [String],
+        }
+    });
+
+    const City = mongoose.model('City', schema);
+
+    City.find({}).then((data) => {
+        cities.push(...data);
+    }).catch((err) => {
+        console.log(err);
+    });
+
+    app.listen(3000, () => {
+        console.log('Server started');
+    });
+}).catch((err) => {
+    console.log(err);
+});
+
+app.get('/', (req, res) => {
+    res.send({cities});
+});
+
+app.get('/:country', async (req, res) => {
+    let country = req.params.country;
+    if(country === 'usa') country = 'USA';
+    if(country === 'india') country = 'India';
+    if(country === 'china') country = 'China';
+    let result = [];
+    result = cities.filter((city) => {
+        return city.country === country;
+    });
+    result = _.map(result, result => _.pick(result, ['city', 'country', 'population', 'climate']));
+    result = _.sortBy(result, ['population']);
+    res.send({cities: result});
+});
