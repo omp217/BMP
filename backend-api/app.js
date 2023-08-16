b3b48e5d3b06fd7e5910a970fff66fedf126b9b7
Devansh Patel <202001262@daiict.ac.in>
2023-08-16 18:26:46 +0530

    modified the mongodb connection and added all the options

--- backend-api/app.js
+++ backend-api/app.js
@@ -4,108 +4,110 @@ const _ = require('lodash');
 
 const app = express();
 
-const dbURL = "mongodb+srv://sherlock:sherlocked221b@cluster0.cfth3qm.mongodb.net/bmp";
+const dbURL = "mongodb+srv://202001262:devansh2292@cluster0.pbqmu9o.mongodb.net/?retryWrites=true&w=majority";
 
 const products = [];
+
+const Schema = mongoose.Schema;
+const schema = new Schema({
+    title: {
+        type: String,
+    },
+    category: {
+        type: String,
+    },
+    platform: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    actual_price: {
+        type: Number,
+    },
+    discount: {
+        type: Number,
+    },
+    five_star: {
+        type: Number,
+    },
+    four_star: {
+        type: Number,
+    },
+    three_star: {
+        type: Number,
+    },
+    two_star: {
+        type: Number,
+    },
+    one_star: {
+        type: Number,
+    },
+});
+
+const product = mongoose.model('product', schema);
+
 
 mongoose.connect(dbURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
     console.log('Connected to MongoDB');
-
-    const Schema = mongoose.Schema;
-
-    const schema = new Schema({
-        title: {
-            type: String,
-        },
-        category: {
-            type: String,
-        },
-        platform: {
-            type: String,
-        },
-        price: {
-            type: Number,
-        },
-        actual_price: {
-            type: Number,
-        },
-        discount: {
-            type: Number,
-        },
-        five_star: {
-            type: Number,
-        },
-        four_star: {
-            type: Number,
-        },
-        three_star: {
-            type: Number,
-        },
-        two_star: {
-            type: Number,
-        },
-        one_star: {
-            type: Number,
-        },
-    });
-
-    const Product = mongoose.model('Product', schema);
-
-    Product.find({}).then((data) => {
-        products.push(...data);
-    }).catch((err) => {
-        console.log(err);
-    });
-
-    app.listen(3000, () => {
-        console.log('Server started');
-    });
-}).catch((err) => {
-    console.log(err);
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
 });
 
-app.get('/', (req, res) => {
-    res.json(products)
+app.listen(3000, () => {
+    console.log('Server started');
 });
+
+app.get('/', async (req, res) => {
+    try {
+        const products = await product.find();
+        res.json(products);
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 
 app.get('/:category/:platform', async (req, res) => {
-    const categories = ['Men', 'Women'];
-    const platforms = ['Amazon', 'Flipkart', 'Snapdeal'];
+    try {
 
-    let category = parseInt(req.params.category);
-    let platform = parseInt(req.params.platform);
+    let { category, platform } = req.params;
 
-    let result = [];
+        let categoryFound = category.split('&');
+        let platformFound = platform.split('&');
 
-    if(category === 0) category = 3;
-    if(platform === 0) platform = 7;
+        for(let i = 0; i < categoryFound.length; i++) {
+            categoryFound[i] = categoryFound[i].charAt(0).toUpperCase() + categoryFound[i].slice(1);
+        }
 
-    let categoryArr = [];
-    for(let i = 0; i < 2; i++) {
-        if(category & (1 << i)) categoryArr.push(categories[i]);
-    }
+        for(let i = 0; i < platformFound.length; i++) {
+            platformFound[i] = platformFound[i].charAt(0).toUpperCase() + platformFound[i].slice(1);
+        }
 
-    let platformArr = [];
-    for(let i = 0; i < 3; i++) {
-        if(platform & (1 << i)) platformArr.push(platforms[i]);
-    }
 
-    result = products.filter((product) => {
-        return categoryArr.includes(product.category) && platformArr.includes(product.platform);
+        console.log(categoryFound, platformFound);
+        let result = await product.find({
+            category: { $in: categoryFound },
+            platform: { $in: platformFound }
     });
 
-    result = result.map((product) => {
-        let rating = (5 * product.five_star + 4 * product.four_star + 3 * product.three_star + 2 * product.two_star + 1 * product.one_star) / (product.five_star + product.four_star + product.three_star + product.two_star + product.one_star);
-        product.rating = rating.toFixed(1);
-        return product;
-    });
+        // Add rating to each product
+        result = result.map((product) => {
+            const rating = (5 * product.five_star + 4 * product.four_star + 3 * product.three_star + 2 * product.two_star + 1 * product.one_star) / (product.five_star + product.four_star + product.three_star + product.two_star + product.one_star);
+            product.rating = rating.toFixed(1);
+            return product;
+        });
 
-    result = _.map(result, result => _.pick(result, ['title', 'category', 'platform', 'price', 'discount', 'rating']));
-    
-    result = _.orderBy(result, ['discount'], ['desc']);
-
-    res.json(result);
+        // Sort products by discount
+        result = _.map(result, result => _.pick(result, ['title', 'category', 'platform', 'price', 'discount', 'rating']));
+        result = _.orderBy(result, ['discount'], ['desc']);
+        res.json(result);
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
