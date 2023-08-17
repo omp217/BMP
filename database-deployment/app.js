aae5760acd4e99732b76bba9b95c3ea284567df8
Om Patel <o.v.patel2705@gmail.com>
2023-08-17 14:41:03 +0530

    Added Image URL in database

--- database-deployment/app.js
+++ database-deployment/app.js
@@ -1,6 +1,29 @@
 const mongoose  = require('mongoose');
 const csv = require('csv-parser');
 const fs = require('fs');
+const axios = require('axios');
+const https = require('https');
+
+let nextPageMen = 'https://api.pexels.com/v1/search/?page=1&per_page=50&query=men';
+let nextPageWomen = 'https://api.pexels.com/v1/search/?page=1&per_page=50&query=women';
+
+let queueMen = [];
+let queueWomen = [];
+
+async function getRandomImage(gender) {
+    let url = gender ? nextPageMen : nextPageWomen;
+    console.log('URL Assigned : ', url);
+    let response = await fetch(url, {
+        headers: {
+            Authorization: 'sFLyghBm0tk8AvxR9JOuOMcRfmtwAM8M5BzcLqRbwci3R1PD7SU7ObAt'
+        }
+    });
+    response = await response.json();
+    gender ? nextPageMen = response.next_page : nextPageWomen = response.next_page;
+    for(let photo of response.photos) {
+        gender ? queueMen.push(photo.url) : queueWomen.push(photo.url);
+    }
+}
 
 const Schema = mongoose.Schema;
 
@@ -46,46 +69,71 @@ mongoose.connect(dbURL, {
         one_star: {
             type: Number,
         },
+        imageurl: {
+            type: String,
+        }
     });
 
     const Product = mongoose.model('Product', schema);
 
     let total = 0;
 
+    let rows = [];
+
     fs.createReadStream('dataset.csv')
     .pipe(csv({ headers: false }))
-    .on('data', async (row) => {
-        let hasUnavailable = false;
-        for (const key in row) {
-            if (row[key] === '' || row[key] === undefined || row[key] === null) {
-                hasUnavailable = true;
-                break;
+    .on('data', (row) => {
+        rows.push(row);
+    })
+    .on('end', async () => {
+        console.log('CSV file successfully processed');
+        for(let row of rows) {
+            let hasUnavailable = false;
+            for (const key in row) {
+                if (row[key] === '' || row[key] === undefined || row[key] === null) {
+                    hasUnavailable = true;
+                    break;
+                }
+            }
+            if(!hasUnavailable) {
+                const object = {
+                    title: row[0],
+                    category: row[1],
+                    platform: row[2],
+                    price: parseFloat(row[3]),
+                    actual_price: parseFloat(row[4]),
+                    discount: parseFloat(row[5].substring(0, row[5].length - 1)),
+                    five_star: parseInt(row[6]),
+                    four_star: parseInt(row[7]),
+                    three_star: parseInt(row[8]),
+                    two_star: parseInt(row[9]),
+                    one_star: parseInt(row[10]),
+                };
+    
+                const platforms = ['Amazon', 'Flipkart', 'Snapdeal'];
+                const randomIndex = Math.floor(Math.random() * platforms.length);
+                object.platform = platforms[randomIndex];
+
+                if(object.category === 'Men') {
+                    if(queueMen.length===0) {
+                        await getRandomImage(true);
+                    }
+                    object.imageurl = queueMen.shift();
+                }
+                else {
+                    if(queueWomen.length===0) {
+                        await getRandomImage(false);
+                    }
+                    object.imageurl = queueWomen.shift();
+                }
+                console.log(object.category, object.imageurl)
+    
+                const product = new Product(object);
+                await product.save();
+                console.log('Saved - ', ++total);
             }
         }
-        if(!hasUnavailable) {
-            const object = {
-                title: row[0],
-                category: row[1],
-                platform: row[2],
-                price: parseFloat(row[3]),
-                actual_price: parseFloat(row[4]),
-                discount: parseFloat(row[5].substring(0, row[5].length - 1)),
-                five_star: parseInt(row[6]),
-                four_star: parseInt(row[7]),
-                three_star: parseInt(row[8]),
-                two_star: parseInt(row[9]),
-                one_star: parseInt(row[10]),
-            };
-
-            const platforms = ['Amazon', 'Flipkart', 'Snapdeal'];
-            const randomIndex = Math.floor(Math.random() * platforms.length);
-            object.platform = platforms[randomIndex];
-
-            const product = new Product(object);
-            await product.save();
-            console.log('Saved - ', ++total);
-        }
-    })
+    });
 }).catch((error) => {
     console.log('Error connecting to MongoDB', error.message);
 });
