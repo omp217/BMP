208f92e6248a7530bf4af551004a2ecdf4e565a5
Om Patel <o.v.patel2705@gmail.com>
2023-08-28 19:23:59 +0530

    Added middleware for solving the cross platform issue

--- backend-api/app.js
+++ backend-api/app.js
@@ -67,6 +67,14 @@ mongoose.connect(dbURL, {
     console.log(err);
 });
 
+app.use(function(req, res, next) {
+    console.log('Here in middleware.')
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => {
     res.json(products)
 });
